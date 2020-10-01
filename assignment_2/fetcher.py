a086b42f5c99bf931a55a858083d0ba9f23e3328
alexannf <alexannf@stud.ntnu.no>
2020-10-01 04:40:21 +0200

    Add functionality for adding Activities, changed Activity id to integer and did some other small fixes. Small breaking change regarding assigning transportation mode, SQL syntax error

--- assignment_2/fetcher.py
+++ assignment_2/fetcher.py
@@ -1,6 +1,4 @@
 import os
-import pprint
-
 
 """
     Data structure:
@@ -8,16 +6,42 @@ import pprint
             {
                 000: (user_id)
                     20081023025304: (activity_id, "plt file name")
-                        [[trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
+                        [<activity start datetime>, <activity end datetime>, 
+                            [trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
                     20081024020959:
-                        [[trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
+                        [<activity start datetime>, <activity end datetime>, 
+                            [trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
                     ...
                 001: (user_id)
+                    ...
+                ...
+                010: (userid) <this user has a label>
+                    'labels': [[label 1], [label 2], ..., [label N]]
+                    20081023025304: (activity_id, "plt file name")
+                        [<activity start datetime>, <activity end datetime>, 
+                            [trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
+                    20081024020959:
+                        [<activity start datetime>, <activity end datetime>, 
+                            [trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
+                    ...
+                011: (user_id)
+                    20081023025304: (activity_id, "plt file name")
+                        [<activity start datetime>, <activity end datetime>, 
+                            [trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
+                    20081024020959:
+                        [<activity start datetime>, <activity end datetime>, 
+                            [trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
                     ...
                 ...
                 181: (user_id)
                     ...
             }
+            
+        label example:
+            ['2007/06/26 11:32:29', '2007/06/26 11:40:29', 'bus']
+            
+        trackpoint example:
+            ['40.127951', '116.48646', '0', '62', '39969.065474537', '2009-06-05', '01:34:17']
 
 """
 
@@ -30,24 +54,27 @@ class Fetcher:
     # Main method: iterates file tree and inserts users, activities and trackpoints
     # Filters out trackpoints with more than 2500 lines (excluding headers)
     def fetch_data(self, num_users=181):
-        current_user = ''
-        iterations = 0  # Used for testing, iterates number of users indicated by num_users
+        user_id = ''
+        activity_id = 0
+        iterations = 0  # num_user is unstable, will give some deterministic number of users close to num_users
         for root, dirs, files in os.walk("dataset"):
             if iterations == num_users + 3:  # + 3 here because three first iterations are uninteresting files/dirs
                 break
             if "Trajectory" in dirs:
-                current_user = root[-3:]
-                print("Adding activities and trackpoints for user with id: {}".format(current_user))
-                self.data[current_user] = {}  # Create directory for each user
+                user_id = root[-3:]
+                print("Adding activities and trackpoints for user with id: {}".format(user_id))
+                self.data[user_id] = {}  # Create directory for each user
 
                 if files:  # Dataset specific, is used to identify if a user has labeled activities
                     labels_filepath = os.path.join(root, files[0])
-                    self.add_labels_to_user(current_user, labels_filepath)
+                    self.add_labels_to_user(user_id, labels_filepath)
 
-            if current_user:  # Dataset specific, skips uninteresting files at higher directory level
-                for name in files:
-                    activity_filepath = os.path.join(root, name)
-                    self.add_activities_and_trackpoints_to_user(current_user, activity_filepath, name[:-4])
+            # Dataset specific, skips uninteresting files at higher directory level
+            if user_id and "Trajectory" not in dirs:
+                for activity in files:
+                    activity_id += 1
+                    activity_filepath = os.path.join(root, activity)
+                    self.add_activities_and_trackpoints_to_user(user_id, activity_filepath, activity_id)
             if files:
                 iterations += 1
         return self.data
@@ -61,17 +88,30 @@ class Fetcher:
             self.data[current_user]['labels'].append(labeled_activity.strip().split("\t"))
 
     # Helper method for fetch_data
-    def add_activities_and_trackpoints_to_user(self, current_user, activity_filepath, activity_id):
+    def add_activities_and_trackpoints_to_user(self, user_id, activity_filepath, activity_id):
         activity_file = open(activity_filepath, "r")
         trackpoints = activity_file.readlines()[6:]
 
         # Ensures that activities with too many trackpoints don't get added
         if sum(1 for trackpoint in trackpoints) > 2500:
             return
-        self.data[current_user][activity_id] = []
-        for trackpoint in trackpoints:
+        self.data[user_id][activity_id] = []
 
-            self.data[current_user][activity_id].append(trackpoint.strip().split("\t"))
+        if trackpoints:
+            start_date = trackpoints[0].strip().split(",")[5]
+            start_time = trackpoints[0].strip().split(",")[6]
+            start_date_time = "{} {}".format(start_date, start_time)
+
+            end_date = trackpoints[len(trackpoints)-1].strip().split(",")[5]
+            end_time = trackpoints[len(trackpoints)-1].strip().split(",")[6]
+            end_date_time = "{} {}".format(end_date, end_time)
+
+            self.data[user_id][activity_id].append(start_date_time)
+            self.data[user_id][activity_id].append(end_date_time)
+
+        for trackpoint in trackpoints:
+            trackpoint_attributes = trackpoint.strip().split(",")
+            self.data[user_id][activity_id].append(trackpoint_attributes)
 
     def get_data(self):
         return self.data
@@ -79,8 +119,7 @@ class Fetcher:
 
 def main():
     fetcher = Fetcher()
-    fetcher.fetch_data(2)
-    # pprint.pp(fetcher.get_data())
+    fetcher.fetch_data(15)
 
 
 if __name__ == '__main__':
--- assignment_2/inserter.py
+++ assignment_2/inserter.py
@@ -10,7 +10,7 @@ class DBTables:
         self.db_connection = self.connection.db_connection
         self.cursor = self.connection.cursor
 
-    def create_tables(self):
+    def create_all_tables(self):
         self.create_user_table()
         self.create_activity_table()
         self.create_trackpoint_table()
@@ -35,7 +35,7 @@ class DBTables:
                     (
                         id INT NOT NULL,
                         user_id CHAR(3) NOT NULL,
-                        transportation_mode VARCHAR(20),
+                        transportation_mode VARCHAR(30),
                         start_date_time DATETIME NOT NULL,
                         end_date_time DATETIME NOT NULL,
                         PRIMARY KEY (id),
@@ -85,23 +85,50 @@ class DBTables:
             query = "DROP TABLE %s"
             self.cursor.execute(query % table)
 
+
 class DBInserter:
     """
         Data structure:
             self.data:
                 {
                     000: (user_id)
-                        20081023025304: (activity_id, "plt file name")
-                            [[trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
-                        20081024020959:
-                            [[trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
+                        1: (activity_id, "plt file name")
+                            [<activity start datetime>, <activity end datetime>,
+                                [trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
+                        2:
+                            [<activity start datetime>, <activity end datetime>,
+                                [trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
                         ...
                     001: (user_id)
+                        ...
+                    ...
+                    010: (userid) <this user has a label>
+                        'labels': [[label 1], [label 2], ..., [label N]]
+                        1239: (activity_id, "plt file name")
+                            [<activity start datetime>, <activity end datetime>,
+                                [trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
+                        1240:
+                            [<activity start datetime>, <activity end datetime>,
+                                [trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
+                        ...
+                    011: (user_id)
+                        1679: (activity_id, "plt file name")
+                            [<activity start datetime>, <activity end datetime>,
+                                [trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
+                        1680:
+                            [<activity start datetime>, <activity end datetime>,
+                                [trackpoint 1], [trackpoint 2], ..., [trackpoint N]]
                         ...
                     ...
                     181: (user_id)
                         ...
                 }
+
+            label example:
+                ['2007/06/26 11:32:29', '2007/06/26 11:40:29', 'bus']
+
+            trackpoint example:
+                ['40.127951', '116.48646', '0', '62', '39969.065474537', '2009-06-05', '01:34:17']
 
     """
 
@@ -113,44 +140,80 @@ class DBInserter:
 
     def insert_data(self):
         for user in self.data:
-            self.insert_user(user)
+            has_labels = 1 if 'labels' in self.data[user] else 0
+            self.insert_user(user, has_labels)
+            for activity in self.data[user]:
+                self.insert_activity(user, activity, has_labels)
 
-    def insert_user(self, user):
-        has_labels = 1 if 'labels' in self.data[user] else 0
+
+    def insert_user(self, user, has_labels):
         query = "INSERT INTO Users VALUES ('{}',{})".format(user, has_labels)
         self.cursor.execute(query)
         self.db_connection.commit()
+
+    def insert_activity(self, user, activity, has_labels):
+        transportation_mode = None
+        start_date_time = self.data[user][activity][0]
+        end_date_time = self.data[user][activity][1]
+        if has_labels:
+            transportation_mode = self.determine_transportation(user, start_date_time, end_date_time)
+        query = "INSERT INTO Activities VALUES ('{}','{}','{}','{}','{}')"\
+            .format(activity, user, transportation_mode, start_date_time, end_date_time)
+        self.cursor.execute(query)
+        self.db_connection.commit()
+
+    def determine_transportation(self, user, start_date_time, end_date_time):
+        labeled_activities = self.data[user]['labels']
+        for activity in labeled_activities:
+            if activity[0] == start_date_time and activity[1] == end_date_time:
+                return activity[2]
+        return None
 
     def fetch_users(self):
         query = "SELECT * FROM Users"
         self.cursor.execute(query)
         rows = self.cursor.fetchall()
+
         # Using tabulate to show the table in a nice way
         print("Data from table Users, tabulated:")
         print(tabulate(rows, headers=self.cursor.column_names))
 
+    def fetch_activities(self):
+        query = "SELECT * FROM Activities"
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+
+        # Using tabulate to show the table in a nice way
+        print("Data from table Users, tabulated:")
+        print(tabulate(rows, headers=self.cursor.column_names))
+
+    def execute_query(self, table, query):
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+
+        # Using tabulate to show the table in a nice way
+        print("Data from table {}, tabulated:".format(table))
+        print(tabulate(rows, headers=self.cursor.column_names))
 
 
 def main():
     fetcher = Fetcher()
-    data_set = fetcher.fetch_data(12)  # Fetches data from 12 users
-    program = None
+    data_set = fetcher.fetch_data(100)  # Fetches data from approximately 100 users
     try:
         table_creator = DBTables()
         table_creator.drop_all_tables()
-        table_creator.create_tables()
+        table_creator.create_all_tables()
 
         data_inserter = DBInserter(data_set)
         data_inserter.insert_data()
-        data_inserter.fetch_users()
 
 
 
     except Exception as e:
         print("ERROR: Failed to use database:", e)
     finally:
-        if program:
-            program.connection.close_connection()
+        table_creator.connection.close_connection()
+        data_inserter.connection.close_connection()
 
 
 if __name__ == '__main__':
