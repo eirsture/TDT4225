3068385e97656f43ac74dc939aef5cc778111440
Mathilde Tillman Hegdal <mathilde.tillman@hotmail.com>
2020-10-04 14:29:45 +0200

    Add query 1-4 and 10

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -8,14 +8,31 @@ def main():
 
     try:
         data_querier = DBQuerier()
-        select_users = "SELECT * FROM Users"
+        # select_users = "SELECT * FROM Users"
         # select_some_activities = "SELECT * FROM Activities WHERE transportation_mode IS NOT NULL"
         # select_some_trackpoints = "SELECT * FROM Trackpoints WHERE id < 20"
-        data_querier.execute_display_query('Users', select_users)
+        # data_querier.execute_display_query('Users', select_users)
         # data_querier.execute_display_query('Activities', select_some_activities)
         # data_querier.execute_display_query('Trackpoints', select_some_trackpoints)
         # delete_035 = "DELETE FROM Users WHERE id='035'"
         # data_querier.execute_delete_query('Users', delete_035)
+
+        # Task 1
+        # print("1) Number of users: ", data_querier.countRows("Users"))
+        #print("1) Number of activities: ", data_querier.countRows("Activities"))
+        #print("1) Number of trackpoints: ", data_querier.countRows("Trackpoints"))
+
+        # Task 2
+        # data_querier.getAverageActivitiesPrUser()
+
+        # Task 3
+        # data_querier.getTop20ActiveUsers()
+
+        # Task 4
+        # data_querier.getUsersTakingTaxi()
+
+        # Task 10
+        # data_querier.getUsersInForbiddenCity()
 
     except Exception as e:
         print("ERROR: Failed to use database:", e)
@@ -25,11 +42,6 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
 
     """ Can be inserted to main if you want to tear down and build up the database again
         especially be careful with dropping the tables"""
