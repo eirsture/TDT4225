8993b30d8cdeb9faff8021f2f9bf070e104de209
eirsture <eirik.sture@gmail.com>
2020-10-05 17:39:29 +0200

    Add task 5,6 and start on 11

--- assignment_2/querier.py
+++ assignment_2/querier.py
@@ -17,6 +17,42 @@ class DBQuerier:
         print("Data from table {}, tabulated:".format(table))
         print(tabulate(rows, headers=self.cursor.column_names))
 
+    def task5(self):
+
+        query = "SELECT transportation_mode, COUNT(transportation_mode) as number_of FROM Activities WHERE transportation_mode IS NOT NULL GROUP BY(transportation_mode);"
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        print("5) Transportation mode and number of activities of that transportation mode")
+        print(tabulate(rows, headers=self.cursor.column_names))
+
+    def task6(self):
+        """ Didn't get MAX() to work so did as suggested by: https://stackoverflow.com/a/16133099 """
+        "SELECT YEAR(start_date_time) as year, COUNT(*) as number_of FROM Activities GROUP BY YEAR(start_date_time) ORDER BY number_of DESC LIMIT 1"
+        query_a = "SELECT YEAR(start_date_time) as year, COUNT(*) as number_of FROM Activities GROUP BY YEAR(start_date_time) ORDER BY number_of DESC LIMIT 1"
+        self.cursor.execute(query_a)
+        rows_a = self.cursor.fetchall()
+        print("6 a) The year with the most activities")
+        print(tabulate(rows_a, headers=self.cursor.column_names), "\n\n")
+
+        query_b = "SELECT YEAR(start_date_time), SUM(end_date_time - start_date_time) as recorded_hours FROM Activities GROUP BY YEAR(start_date_time) ORDER BY recorded_hours DESC LIMIT 1"
+        self.cursor.execute(query_b)
+        rows_b = self.cursor.fetchall()
+        print("6 b) The year with the most recorded hours")
+        print(tabulate(rows_b, headers=self.cursor.column_names), "\n\n")
+        if (rows_a[0][0] == rows_b[0][0]):
+            print("As you can see was " + str(rows_a[0][0]) + " both the year with most activities and the most recorded hours.")
+        else:
+            print("As you can see was " + str(rows_a[0][0]) + " the year with most activities, while " + str(rows_b[0][0]) + " was the year with the most recorded hours.")
+
+
+
+    def task11(self):
+        query = "SELECT id FROM Users WHERE has_labels=TRUE"
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+        print("6 a) The year with the most activities and number of")
+        print(tabulate(rows, headers=self.cursor.column_names))
+
     # def execute_delete_query(self, table, query):
     #     self.cursor.execute(query)
     #     self.db_connection.commit()
