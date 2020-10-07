04fb22d58d11ab3df9583781e1173300702921dc
Johannes Åsheim <johannes.aasheim@hotmail.com>
2020-10-07 20:37:02 +0200

    Q7 og Q8 (#6)
    Co-authored-by: Johannes Åsheim <joh.aasheim@gmail.com>
    Co-authored-by: eirsture <37185897+eirsture@users.noreply.github.com>

--- assignment_2/querier.py
+++ assignment_2/querier.py
@@ -1,6 +1,6 @@
 from dbConnector import DbConnector
 from tabulate import tabulate
-
+from haversine import haversine
 
 class DBQuerier:
 
@@ -70,6 +70,7 @@ class DBQuerier:
         rows_a = self.cursor.fetchall()
         print("6 a) The year with the most activities")
         print(tabulate(rows_a, headers=self.cursor.column_names), "\n\n")
+        
         query_b = "Select YEAR(start_date_time) as year, SUM(TIMEDIFF(end_date_time, start_date_time))/3600 as recorded_hours FROM Activities GROUP BY year ORDER BY recorded_hours DESC LIMIT 1"
         self.cursor.execute(query_b)
         rows_b = self.cursor.fetchall()
@@ -79,7 +80,45 @@ class DBQuerier:
             print("As you can see was " + str(rows_a[0][0]) + " both the year with most activities and the most recorded hours.")
         else:
             print("As you can see was " + str(rows_a[0][0]) + " the year with most activities, while " + str(rows_b[0][0]) + " was the year with the most recorded hours.")
+  
+    # Task 7
+    def total_distance_walked(self):
+        # Find the total distance (in km) walked in 2008, by user with id=112
+        
+        points = []
 
+        
+        act_id = "SELECT id FROM Activities WHERE user_id=112"
+
+        self.cursor.execute(act_id)
+        rows = self.cursor.fetchall()
+
+        for row in rows:
+            trc = "SELECT lat, lon FROM Trackpoints WHERE activity_id =" + str(row[0])
+
+            self.cursor.execute(trc)
+            trc_rows = self.cursor.fetchall()
+
+            km = 0
+
+            for i in range(len(trc_rows)-1):
+                a = trc_rows[i]
+                b = trc_rows[i+1]
+                km += haversine(a,b)
+
+            points.append(km)
+        
+        sum_points = sum(points)
+        print("Distance walked by user_id=112: ", sum_points)
+
+    # Task 8
+    def top_gained_altitude(self):
+        # Find the top 20 users who have gained the most altitude meters
+
+        query =  "SELECT Activities.user_id, SUM(To_points.altitude - From_points.altitude) / 3.2808 AS Altitude_gained FROM Trackpoints AS From_points INNER JOIN Trackpoints AS To_points ON From_points.id = To_points.id - 1 AND From_points.activity_id = To_points.activity_id AND To_points.altitude > From_points.altitude INNER JOIN Activities ON From_points.activity_id = Activities.id GROUP BY Activities.user_id ORDER BY Altitude_gained DESC LIMIT 20"
+                
+        self.execute_display_query("Activities", query)
+    
     # Task 10
     # Slow
     def getUsersInForbiddenCity(self):
