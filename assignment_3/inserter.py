8f5ebad23025d3a9fe925233941fec7558d82307
eirsture <eirik.sture@gmail.com>
2020-10-18 13:03:54 +0200

    finished inserter

--- assignment_3/inserter.py
+++ assignment_3/inserter.py
@@ -54,14 +54,16 @@ class DBInserter:
         self.trackpoints = []
 
     def prepare_users(self):
+        trackpoint_id = 0
         for user in self.data:
             has_labels = 1 if user in self.labels else 0
             self.users.append({"_id": user, "has_labels": has_labels})
 
             for activity in self.data[user]:
                 self.prepare_activities(activity, user)
+                trackpoint_id = self.prepare_trackpoints(user, activity, trackpoint_id)
 
-    def prepare_activities(self, activity, user):
+    def prepare_activities(self, user, activity):
         start_date_time = self.data[user][activity][0]
         end_date_time = self.data[user][activity][1]
         transportation_mode = self.data[user][activity][2]
@@ -73,41 +75,39 @@ class DBInserter:
             "end_date_time": end_date_time
         })
 
-
-    def insert_data(self):
-        for user in self.data:
-            has_labels = 1 if user in self.labels else 0
-            self.insert_user(user, has_labels)
-            print("Inserting activities for user with id: {}".format(user))
-            for activity in self.data[user]:
-                self.insert_activity(user, activity)
-                self.insert_trackpoints(user, activity)
-
-    def insert_user(self, user, has_labels):
-        query = "INSERT INTO Users VALUES ('{}',{})".format(user, has_labels)
-        self.cursor.execute(query)
-        self.db_connection.commit()
-
-    def insert_activity(self, user, activity):
-        start_date_time = self.data[user][activity][0]
-        end_date_time = self.data[user][activity][1]
-        transportation_mode = self.data[user][activity][2]
-        query = "INSERT INTO Activities VALUES ('{}','{}',{},'{}','{}')"\
-            .format(activity, user, transportation_mode, start_date_time, end_date_time)
-        self.cursor.execute(query)
-        self.db_connection.commit()
-
-    def insert_trackpoints(self, user, activity):
+    def prepare_trackpoint(self, user, activity, trackpoint_id):
         trackpoints = self.data[user][activity][3:]
-        query = "INSERT INTO Trackpoints (activity_id, lat, lon, altitude, date_days, date_time) VALUES "
 
-        # Build SQL query so that we only do one insert with a lot of rows
         for trackpoint in trackpoints:
             lat, lon, altitude = trackpoint[0], trackpoint[1], trackpoint[2]
             date_days, date_time = trackpoint[3], trackpoint[4]
-            query += "('{}', '{}', '{}', '{}', '{}', '{}'),"\
-                .format(activity, lat, lon, altitude, date_days, date_time)
-        final_query = query[:-1]
-        self.cursor.execute(final_query)
-        self.db_connection.commit()
+            self.trackpoints.append({
+                "_id": trackpoint_id,
+                "activity_id": activity,
+                "lat": lat,
+                "lon": lon,
+                "altitude": altitude,
+                "date_days": date_days,
+                "date_time": date_time
+
+            })
+            trackpoint_id += 1
+        return trackpoint_id
+
+    def insert_data(self):
+        self.insert_user()
+        self.insert_activity()
+        self.insert_trackpoints()
+
+    def insert_user(self):
+        collection = self.db["User"]
+        collection.insert_many(self.users)
+
+    def insert_activity(self):
+        collection = self.db["Activity"]
+        collection.insert_many(self.activities)
+
+    def insert_trackpoints(self):
+        collection = self.db["Trackpoint"]
+        collection.insert_many(self.trackpoints)
 
