120f5f9f0ea259f4f36d6476740f295d8c3df34d
Johannes Åsheim <joh.aasheim@gmail.com>
2020-10-18 15:48:18 +0200

    Added task 2 and 3

--- assignment_3/querier.py
+++ assignment_3/querier.py
@@ -1,6 +1,6 @@
 from db_connector import DbConnector
 from tabulate import tabulate
-import pprint
+from pprint import pprint
 from haversine import haversine
 
 class DBQuerier:
@@ -28,7 +28,24 @@ class DBQuerier:
         print("Number of trackpoints: ", coll_tp.count())
 
     def q2(self):
+        coll_user = self.db["User"]
+
         coll_act = self.db["Activity"]
-        numUsers = db.User.count()
-        numActivities = db.Activity.count()
+        
+        numUsers = coll_user.count()
+        numActivities = coll_act.count()
         averageNumActivites = numActivities/numUsers
+        print("Average number of activities pr user: ", averageNumActivites)
+
+    def q3(self): 
+        coll_act = self.db["Activity"]
+
+        pipeline = [
+            { "$group": { "_id": "$user", "NumberOfActivities": { "$sum": 1 }}},
+		    { "$sort": { "NumberOfActivities": -1 } },
+            { "$limit": 20 }
+        ]
+        documents = coll_act.aggregate(pipeline)
+
+        for doc in documents: 
+            pprint(doc)
